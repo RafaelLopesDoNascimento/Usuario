77c1afb611ab84e0342ab65e24f42b052bd03075
Rafael Lopes <rafaellopesdonascimento2607@gmail.com>
2023-07-04 21:21:12 -0300

    Alterando senha de usuario com o POO

--- src/User/Usuario.java
+++ src/User/Usuario.java
@@ -1,2 +1,38 @@
-package User;public class Usuario {
+package User;
+
+public class Usuario {
+
+    //Propriedades
+    private String nome;
+    private short senha;
+
+
+    //constructor
+
+public Usuario(String nome, short senha) {
+    this.nome = nome;
+    this.senha = senha;
+}
+
+
+    //metodos
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public short getSenha() {
+        return senha;
+    }
+
+    public void alterarSenha(String nome, Short senhaNova) {
+    if(nome == this.nome) {
+        this.senha = senhaNova;
+        System.out.println("Senha alterada");
+
+    } else {
+        System.out.println("Nome de usuario não encontrado");
+    }
+    }
 }
